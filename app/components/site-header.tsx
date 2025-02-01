30de4d59188dcb1e72c9818bb48a043259328b16
kiv1n <aloha@kiv1n.ru>
2025-02-01 15:11:15 +0300

    Set up base components for the app

---
+++ app/components/site-header.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { siteConfig } from "@/config/site";
+import { MomIcon } from "@/icons/mom";
+import { GithubIcon, MenuIcon } from "lucide-react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+
+export function SiteHeader() {
+  return (
+    <header className="sticky top-0 z-40 bg-card border-b">
+      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+        {/* Left part */}
+        <div className="flex gap-6 md:gap-10">
+          <Link href="/" className="flex items-center space-x-2">
+            <MomIcon className="text-foreground size-8" />
+            <span className="text-foreground font-bold">{siteConfig.name}</span>
+          </Link>
+        </div>
+        {/* Right part */}
+        <div className="flex flex-1 items-center justify-end gap-4">
+          <DropdownMenu>
+            <DropdownMenuTrigger
+              className="text-sm font-medium text-muted-foreground"
+              asChild
+            >
+              <Button variant="ghost" size="icon">
+                <MenuIcon />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <Link href={siteConfig.links.github} target="_blank">
+                <DropdownMenuItem>
+                  <GithubIcon />
+                  <span>GitHub</span>
+                </DropdownMenuItem>
+              </Link>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+    </header>
+  );
+}
---
+++ app/config/site.ts
@@ -0,0 +1,10 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Agent 4 Your Mom",
+  description: "...", // TODO: Fill in
+  links: {
+    github: "https://github.com/web3goals/agent-4-your-mom-prototype",
+    twitter: "https://twitter.com/kiv1n",
+  },
+};
