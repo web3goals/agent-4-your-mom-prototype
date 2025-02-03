c73e5558494243811e69e9d8b2e10479103a3fca
kiv1n <aloha@kiv1n.ru>
2025-02-03 16:39:03 +0300

    Fill in site description

--- app/config/site.ts
+++ app/config/site.ts
@@ -2,7 +2,8 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: "Agent 4 Your Mom",
-  description: "...", // TODO: Fill in
+  description:
+    "A platform for creating and using personalized AI agents with crypto features for your mom or other not-techie users",
   links: {
     github: "https://github.com/web3goals/agent-4-your-mom-prototype",
     twitter: "https://twitter.com/kiv1n",
