9be9ec1cf3a9ea7484a7e069f91cf3ba7a951dd7
kiv1n <aloha@kiv1n.ru>
2025-02-03 16:38:51 +0300

    Implement landing page

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,8 +1,34 @@
-// TODO: Implement
+import { Button } from "@/components/ui/button";
+import { BotIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function LandingPage() {
   return (
     <main className="container py-6 lg:px-80">
-      <p>Landing page...</p>
+      <div className="flex flex-col items-center">
+        <Image
+          src="/images/cover.png"
+          alt="Cover"
+          priority={false}
+          width="100"
+          height="100"
+          sizes="100vw"
+          className="w-full rounded-xl"
+        />
+      </div>
+      <h1 className="text-4xl md:text-5xl font-extrabold tracking-tighter max-w-[680px] mt-4">
+        Help your mom venture into the crypto world with a personalized AI agent{" "}
+      </h1>
+      <p className="font-medium tracking-tight text-muted-foreground max-w-[680px] mt-2">
+        A platform for creating and using personalized AI agents with crypto
+        features for your mom or other not-techie users
+      </p>
+      <Link href="/agents/new">
+        <Button className="mt-4">
+          <BotIcon /> Create agent
+        </Button>
+      </Link>
     </main>
   );
 }
