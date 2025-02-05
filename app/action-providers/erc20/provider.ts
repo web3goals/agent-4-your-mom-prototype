40553da1fef0a881343ee6ffbc7ffc910c755be4
kiv1n <aloha@kiv1n.ru>
2025-02-05 11:22:00 +0300

    Add empty address book action provider, update erc20 action provider

--- app/action-providers/erc20/provider.ts
+++ app/action-providers/erc20/provider.ts
@@ -1,4 +1,3 @@
-import { Agent } from "@/mongodb/models/agent";
 import {
   ActionProvider,
   CreateAction,
@@ -7,18 +6,11 @@ import {
 import { encodeFunctionData, Hex } from "viem";
 import { z } from "zod";
 import { abi } from "./constants";
-import {
-  GetBalanceSchema,
-  TransferByNameSchema,
-  TransferSchema,
-} from "./schemas";
+import { GetBalanceSchema, TransferSchema } from "./schemas";
 
 export class ERC20ActionProvider extends ActionProvider {
-  agent: Agent;
-
-  constructor(agent: Agent) {
+  constructor() {
     super("erc20", []);
-    this.agent = agent;
   }
 
   /**
@@ -52,6 +44,8 @@ This tool will get the balance of an ERC20 asset in the wallet. It takes the con
       return `Error getting balance: ${error}`;
     }
   }
+
+  // TODO: Don't send money if address doesn't exist in address book
 
   /**
    * Transfers a specified amount of an ERC20 token to a destination onchain.
@@ -98,67 +92,66 @@ This tool will get the balance of an ERC20 asset in the wallet. It takes the con
     }
   }
 
+  //   /**
+  //    * Transfers a specified amount of an ERC20 token to a person or organization by their name.
+  //    *
+  //    * @param walletProvider - The wallet provider to transfer the asset from.
+  //    * @param args - The input arguments for the action.
+  //    * @returns A message containing the transfer details.
+  //    */
+  //   @CreateAction({
+  //     name: "transfer_by_name",
+  //     description: `
+  // This tool will transfer an ERC20 token from the wallet to another person or organization onchain.
+
+  // It takes the following inputs:
+  // - amount: The amount to transfer
+  // - contractAddress: The contract address of the token to transfer
+  // - destinationName: Whom to send the funds (can be a name of person or organization)
+
+  // Important notes:
+  // - Ensure sufficient balance of the input asset before transferring
+  // - When sending native assets (e.g. 'eth' on base-mainnet), ensure there is sufficient balance for the transfer itself AND the gas cost of this transfer
+  //     `,
+  //     schema: TransferByNameSchema,
+  //   })
+  //   async transferByName(
+  //     walletProvider: EvmWalletProvider,
+  //     args: z.infer<typeof TransferByNameSchema>
+  //   ): Promise<string> {
+  //     try {
+  //       // Define destination by name using address book from Nillion SecretVault
+  //       // TODO:
+  //       const destination = "";
+  //       if (!destination) {
+  //         return "Failed to find an address for the specified name";
+  //       }
+
+  //       // Send transaction
+  //       const hash = await walletProvider.sendTransaction({
+  //         to: args.contractAddress as Hex,
+  //         data: encodeFunctionData({
+  //           abi,
+  //           functionName: "transfer",
+  //           args: [args.destinationName as Hex, BigInt(args.amount)],
+  //         }),
+  //       });
+  //       await walletProvider.waitForTransactionReceipt(hash);
+
+  //       return `Transferred ${args.amount} of ${args.contractAddress} to ${args.destinationName}.\nTransaction hash for the transfer: ${hash}`;
+  //     } catch (error) {
+  //       return `Error transferring the asset: ${error}`;
+  //     }
+  //   }
+
   /**
-   * Transfers a specified amount of an ERC20 token to a person or organization by their name.
+   * Checks if the action provider supports the given network.
    *
-   * @param walletProvider - The wallet provider to transfer the asset from.
-   * @param args - The input arguments for the action.
-   * @returns A message containing the transfer details.
-   */
-  @CreateAction({
-    name: "transfer_by_name",
-    description: `
-This tool will transfer an ERC20 token from the wallet to another person or organization onchain.
-
-It takes the following inputs:
-- amount: The amount to transfer
-- contractAddress: The contract address of the token to transfer
-- destinationName: Whom to send the funds (can be a name of person or organization)
-
-Important notes:
-- Ensure sufficient balance of the input asset before transferring
-- When sending native assets (e.g. 'eth' on base-mainnet), ensure there is sufficient balance for the transfer itself AND the gas cost of this transfer
-    `,
-    schema: TransferByNameSchema,
-  })
-  async transferByName(
-    walletProvider: EvmWalletProvider,
-    args: z.infer<typeof TransferByNameSchema>
-  ): Promise<string> {
-    try {
-      // Define destination by name using Nillion SecretVault
-      // TODO:
-      const destination = "";
-      if (!destination) {
-        return "Failed to find an address for the specified name";
-      }
-
-      // Send transaction
-      const hash = await walletProvider.sendTransaction({
-        to: args.contractAddress as Hex,
-        data: encodeFunctionData({
-          abi,
-          functionName: "transfer",
-          args: [args.destinationName as Hex, BigInt(args.amount)],
-        }),
-      });
-      await walletProvider.waitForTransactionReceipt(hash);
-
-      return `Transferred ${args.amount} of ${args.contractAddress} to ${args.destinationName}.\nTransaction hash for the transfer: ${hash}`;
-    } catch (error) {
-      return `Error transferring the asset: ${error}`;
-    }
-  }
-
-  /**
-   * Checks if the ERC20 action provider supports the given network.
-   *
-   * @returns True if the ERC20 action provider supports the network, false otherwise.
+   * @returns True if the action provider supports the network, false otherwise.
    */
   supportsNetwork = () => {
     return true;
   };
 }
 
-export const erc20ActionProvider = (agent: Agent) =>
-  new ERC20ActionProvider(agent);
+export const erc20ActionProvider = () => new ERC20ActionProvider();
